58c7d0fa4bcc1ce6b8791e71fd7eb2a1cf0ded23
Leroy <leroy_osorio@yahoo.com>
2021-12-04 11:19:04 +0530

    Add new test

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -5,10 +5,15 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import pages.Homepage;
+import pages.SearchResults;
 
 public class Tests {
     
@@ -36,6 +41,21 @@ public class Tests {
     @Test
     public void assertPopupPresent() {
 	Assert.assertEquals(driver.getTitle(), "Petstore.com");
+    }
+    
+    @Test
+    public void performSearch() {
+	Homepage homepage = new Homepage(driver);
+	homepage.search.click();
+	
+	WebDriverWait wait = new WebDriverWait(driver, 5);
+	wait.until(ExpectedConditions.visibilityOf(homepage.searchInput));
+	
+	homepage.searchInput.sendKeys("Dog paw");
+	homepage.submit.click();
+	
+	SearchResults searchResults = new SearchResults(driver);
+	Assert.assertTrue(searchResults.firstSearchResult.isDisplayed());
     }
 
 }
